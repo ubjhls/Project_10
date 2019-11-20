3036a3251de514a7a03f9d0b06b970a2c35a7820
kimseonghoon <ubjhls@naver.com>
2019-11-20 13:38:32 +0900

    account detail 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path('<int:user_pk>/', views.detail, name='detail'),
-    # path('login/', views.login, name='login'),
-    # path('logout/', views.logout, name='logout'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -17,6 +17,8 @@ def index(request):
     return render(request,'accounts/index.html',context)
 
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('accounts:index')
     if request.method == 'POST': 
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
@@ -36,3 +38,24 @@ def detail(request, user_pk):
         'user_profile' : user
     }
     return render(request,'accounts/detail.html', context)
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('accounts:index')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request,user)
+            return redirect('accounts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request,'accounts/signup.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:index')
+
