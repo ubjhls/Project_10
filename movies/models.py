1020b80c3464f01f88f8283ff5efa9294d2a8755
sht3898 <sehyunteg@naver.com>
2019-11-20 13:50:18 +0900

    migrate

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=30)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=40)
+    audience = models.IntegerField()
+    poster_url = models.CharField(max_length=100)
+    description =  models.TextField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='like_movies',
+        blank=True
+        )
+
+class Review(models.Model):
+    content = models.CharField(max_length=100)
+    score = models.IntegerField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
